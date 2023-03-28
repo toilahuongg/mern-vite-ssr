026ad240391e0cdd13e282f4e4cdd4d94a791664
Hugon <hugon@MacBook-Air-cua-Hugon.local>
2023-03-28 23:34:08 +0700

    Add support for left and center aligned headings in Layout/Heading component

--- src/components/Layout/Heading/index.tsx
+++ src/components/Layout/Heading/index.tsx
@@ -1,14 +1,16 @@
 type TProps = {
   title: string;
+  variant?: 'left' | 'center' | 'right';
 };
-const Heading: React.FC<TProps> = ({ title }) => {
+const Heading: React.FC<TProps> = ({ title, variant = 'left' }) => {
   return (
     <div className="flex items-center gap-4">
+      {['right', 'center'].includes(variant) && <div className="flex-1 h-1p bg-primary" />}
       <div className="text-3_5xl font-medium">
         <span className="text-primary">#</span>
         <span className="text-white">{title}</span>
       </div>
-      <div className="flex-1 h-1p bg-primary" />
+      {['left', 'center'].includes(variant) && <div className="flex-1 h-1p bg-primary" />}
     </div>
   );
 };
